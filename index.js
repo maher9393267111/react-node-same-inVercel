9ec05a575fd90f8e9cd02b341ae15a1a87e9bcbd
maher <gomemahero.com>
2022-08-15 02:20:56 +0300

    new branch start

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.get("/api/test", (req, res) => {
-  res.send("test");
+  res.send("test is work good");
 });
 
 app.use(express.static(path.join(__dirname, "./frontend/build")));
